d3efe911a440b75b265e9077523841f12bc7b3c6
Vikas Pal <168359727+PalVikas07@users.noreply.github.com>
2024-12-31 22:26:09 +0530

    Add files via upload

---
+++ Student_Controller.java
@@ -0,0 +1,20 @@
+package com.ty.student.controller;
+
+import com.ty.student.dao.Student_Dao;
+import com.ty.student.dto.Student;
+
+public class Student_Controller {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setId(102);
+		student.setName("pratik");
+		student.setMno(87878787);
+		student.setAddress("thane");
+
+		Student_Dao dao = new Student_Dao();
+//	dao.saveStudent(student);
+		// dao.updateStudent(student);
+		// dao.findStudent(101);
+		dao.deleteStudent(102);
+	}
+}
